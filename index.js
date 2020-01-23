95187faa44391a7531c2bb75f20b38ef3e2fb46a
lukasjaronis <jaronisluke13@gmail.com>
2020-01-23 17:29:44 -0500

    knexfile added + other folder structure

---
+++ index.js
@@ -0,0 +1,8 @@
+const server = require('./server');
+
+const port = process.env.port || 5000;
+
+server.listen(port, () => {
+    console.log(`Server is ONLINE at ${port}`)
+});
+
---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const server = express();
+const recipesRouter = require('../recipes/recipes-router');
+
+server.use(express.json());
+server.use('/api/recipes', recipesRouter);
+
+server.get('/', (request, response) => {
+    response.status(200).send(`<h1>Recipe Book API is ONLINE.</h1>`)
+})
+
+module.exports = server;
