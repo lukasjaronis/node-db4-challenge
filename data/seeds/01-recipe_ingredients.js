142ea8fbf9dcdbe29804e8fdf91f99765766f244
lukasjaronis <jaronisluke13@gmail.com>
2020-01-23 21:02:54 -0500

    recipe_ingredients done

--- data/seeds/01-recipe_ingredients.js
+++ data/seeds/01-recipe_ingredients.js
@@ -1,13 +1,15 @@
-
-exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-    });
-};
+exports.seed = function(knex, Promise) {
+  return knex('recipe_ingredients').insert([
+    { id: 1, recipe_id: 1, ingredient_id: 1, quantity: 3},
+    { id: 2, recipe_id: 1, ingredient_id: 2, quantity: 1.6},
+    { id: 3, recipe_id: 1, ingredient_id: 3, quantity: 7},
+    //
+    { id: 4, recipe_id: 2, ingredient_id: 4, quantity: 4.3},
+    { id: 5, recipe_id: 2, ingredient_id: 5, quantity: 1},
+    { id: 6, recipe_id: 2, ingredient_id: 6, quantity: 8},
+//
+    { id: 7, recipe_id: 3, ingredient_id: 7, quantity: 30},
+    { id: 8, recipe_id: 3, ingredient_id: 8, quantity: 2},
+    { id: 9, recipe_id: 3, ingredient_id: 9, quantity: 0.392}
+  ])
+}
