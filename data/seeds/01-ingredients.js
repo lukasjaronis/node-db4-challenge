4d8bb6418c5df82c4f8dd422464a33e5c75c32ba
lukasjaronis <jaronisluke13@gmail.com>
2020-01-23 20:57:20 -0500

    seed ingredients done

--- data/seeds/01-ingredients.js
+++ data/seeds/01-ingredients.js
@@ -1,13 +1,14 @@
-
-exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-    });
-};
+exports.seed = function(knex, Promise) {
+  return knex('ingredients').insert([
+    { id: 1, name: 'Ing 1'},
+    { id: 2, name: 'Ing 2'},
+    { id: 3, name: 'Ing 3'},
+    { id: 4, name: 'Ing 4'},
+    { id: 5, name: 'Ing 5'},
+    { id: 6, name: 'Ing 6'},
+    { id: 7, name: 'Ing 7'},
+    { id: 8, name: 'Ing 8'},
+    { id: 9, name: 'Ing 9'},
+    { id: 10, name: 'Ing 10'},
+  ])
+}
